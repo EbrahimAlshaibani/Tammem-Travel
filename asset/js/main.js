5f9b62e798fd27b1d5f47d9b24bb15ec17cd6ea5
EbrahimAlshaibani <87685172+EbrahimAlshaibani@users.noreply.github.com>
2022-07-01 09:49:50 +0300

    3

--- asset/js/main.js
+++ asset/js/main.js
@@ -86,7 +86,7 @@ $(function() {
 
 });
 
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -106,12 +106,15 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
 	slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
 	dots[i].className = dots[i].className.replace(" myactive", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " myactive";
-}
+  setTimeout(showSlides, 2000);
 
+}
 
 
